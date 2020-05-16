a4b35d34c86d084c1480e48e64582d1b2c143b6d
SupinePandora43 <supinepandoraggg@gmail.com>
2020-05-16 15:30:40 +0500

    Update coffin_death.lua

--- lua/autorun/server/coffin_death.lua
+++ lua/autorun/server/coffin_death.lua
@@ -1,38 +1,28 @@
 hook.Add("PlayerDeathSound", "DeFlatline", function() return true end)
 local deathsounds = {}
-if file.Exists("coffin/1.wav", "GAME") then
-    table.add(deathsounds, {Sound("coffin_death/1.wav"), Sound("coffin_death/2.wav"), Sound("coffin_death/3.wav")})
+if file.Exists("sound/coffin/1.wav", "GAME") then
+	table.Add(deathsounds, {
+		Sound("coffin/1.wav"), Sound("coffin/2.wav"), Sound("coffin/3.wav")
+	})
 end
-if file.Exists("coffin/medieval.wav", "GAME") then
-    table.insert(deathsounds, Sound("coffin_death/medieval.wav"))
+if file.Exists("sound/coffin/medieval.wav", "GAME") then
+	table.insert(deathsounds, Sound("coffin/medieval.wav"))
 end
-if file.Exists("coffin/minecraft.wav", "GAME") then
-    table.insert(deathsounds, Sound("coffin_death/minecraft.wav"))
+if file.Exists("sound/coffin/minecraft.wav", "GAME") then
+	table.insert(deathsounds, Sound("coffin/minecraft.wav"))
 end
---local deads = {}
 hook.Add("PlayerSpawn", "Coffin_dance", function(ply, _)
-    --for _, ply1 in pairs(deads) do
-    --    if table.HasValue(deads, ply) then
-            ply:StopSound("coffin/1.wav")
-            ply:StopSound("coffin/2.wav")
-            ply:StopSound("coffin/3.wav")
-            ply:StopSound("coffin/medieval.wav")
-            ply:StopSound("coffin/minecraft.wav")
-            --table.RemoveByValue(deads, ply)
-    --    end
-    --end
+	ply:StopSound("coffin/1.wav")
+	ply:StopSound("coffin/2.wav")
+	ply:StopSound("coffin/3.wav")
+	ply:StopSound("coffin/medieval.wav")
+	ply:StopSound("coffin/minecraft.wav")
 end)
 hook.Add("PlayerDeath", "Coffin_dance", function(ply, _, _)
-    --for _, ply in pairs(deads) do
-    --    if ply == v then
-            ply:StopSound("coffin/1.wav")
-            ply:StopSound("coffin/2.wav")
-            ply:StopSound("coffin/3.wav")
-            ply:StopSound("coffin/medieval.wav")
-            ply:StopSound("coffin/minecraft.wav")
-    --        table.RemoveByValue(deads, v)
-    --    end
-    --end
-    v:EmitSound(deathsounds[math.random(#deathsounds)])
-    --table.insert(deads, v)
+	ply:StopSound("coffin/1.wav")
+	ply:StopSound("coffin/2.wav")
+	ply:StopSound("coffin/3.wav")
+	ply:StopSound("coffin/medieval.wav")
+	ply:StopSound("coffin/minecraft.wav")
+	ply:EmitSound(deathsounds[math.random(#deathsounds)])
 end)
